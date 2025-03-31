a5b4af62a431d353c67b3ff8c8bf17f90c07c3b7
Mumtahin Farabi <mfarabi619@gmail.com>
2025-03-31 14:29:45 -0400

    feat(docs): create project

---
+++ apps/docs/next.config.mjs
@@ -0,0 +1,42 @@
+import { createMDX } from 'fumadocs-mdx/next';
+
+const defaultImageHostnames = [
+  'avatars.githubusercontent.com',
+  'raw.githubusercontent.com',
+'media.licdn.com',
+  'www.linuxfoundation.org',
+  'icons.veryicon.com',
+  'logowik.com',
+  'trunkbaseddevelopment.com',
+  'feature-sliced.design',
+  'diataxis.fr',
+  'conventionalcomments.org',
+  'lh3.googleusercontent.com',
+  'res.cloudinary.com',
+  'github.com',
+  'automate.org',
+  'assets.ubuntu.com',
+  'freebsd.org',
+  'caksoylar.github.io',
+  'precondition.github.io',
+  'keymapdb.com',
+  '12factor.net',
+  'uo.zone',
+  'serenity-js.org',
+]
+
+const withMDX = createMDX();
+
+/** @type {import('next').NextConfig} */
+const config = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: defaultImageHostnames.map(hostname => ({
+      protocol: 'https',
+      // eslint-disable-next-line node/prefer-global/process
+      hostname,
+    })),
+  },
+};
+
+export default withMDX(config);
---
+++ apps/docs/src/app/layout.config.tsx
@@ -0,0 +1,48 @@
+import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import type { HomeLayoutProps } from 'fumadocs-ui/layouts/home'
+import Image from 'next/image'
+
+import {
+  Figma as BrandIcon,
+    SquareKanban
+} from 'lucide-react'
+
+export const baseOptions: BaseLayoutProps = {
+  nav: {
+    title: (
+      <>
+        <Image src="/inferno_symbol_3.png" alt="Inferno Logo" height={48} width={48} />
+        <span className="text-lg font-bold text-red-500">
+          Inferno
+          {' '}
+          <span className="text-amber-500">Docs</span>
+        </span>
+      </>
+    ),
+    url: '/',
+  },
+  links: [
+    {
+      text: 'Skytrac Hackathon',
+      url: 'https://www.skytrac.ca/skytrac-hack-a-thon/',
+      external: true,
+    },
+    {
+      type: 'icon',
+      text: 'Brand',
+      url: 'https://design.penpot.app/#/view?file-id=14b4ef9d-aa9c-815a-8005-e3d2794ec6e0&page-id=14b4ef9d-aa9c-815a-8005-e3d2794ec6e1&section=interactions&index=0&share-id=4bbd81c9-0a59-81dd-8005-ee44efe4f14e',
+      external: true,
+      icon: <BrandIcon />,
+    },
+    {
+      type: 'icon',
+      text: 'Project Board',
+      url: 'https://github.com/users/MFarabi619/projects/27/views/1',
+      external: true,
+      icon: <SquareKanban />,
+    },
+  ],
+  githubUrl: 'https://github.com/mira-amm/mira-amm-web',
+};
+
+export const homeOptions: HomeLayoutProps = { ...baseOptions }
